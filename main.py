1a54d6db72b1469d5f5ce992b95c16334a0964e5
Chelsea Alambuya <64859402+alambuyachelsea@users.noreply.github.com>
2024-06-30 18:40:19 +0200

    formatting

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import network
 import time
-import urequests as requests  # MicroPython's urequests for HTTP
-import machine
+import urequests as requests
 import json
 from ultrasonic_sensor import UltrasonicSensor
 from plant_profile import PlantProfile
 
 # Wi-Fi Credentials
-ssid = "Chelsea's S23 Ultra"
-password = 'forfssake'
+ssid = "netwrok name"
+password = 'network password'
 
 # Connect to Wi-Fi
 wlan = network.WLAN(network.STA_IF)
@@ -24,17 +23,16 @@ print('IP Address:', wlan.ifconfig()[0])
 
 # HTTP endpoint (replace with your server URL)
 # Change URL where the server receives data
-server_url = 'http://192.168.151.179:8000/receive_data'
+server_url = 'http://serverURL:serverPortNumber/receive_data'
 
 # Plant profiles with sensor pins
-plants = [
+plants = [  # Change according to the number and needs of the plants
     PlantProfile(name="Basil", water_needs=2, pump_pin=14,
                  sensor_pin=28, img_source='https://i.imgur.com/w8xuEfx.gif'),
     PlantProfile(name="Aloe", water_needs=1, pump_pin=13,
                  sensor_pin=27, img_source='https://i.imgur.com/e9EikGU.gif'),
     PlantProfile(name="Pink Begonia", water_needs=3, pump_pin=15,
                  sensor_pin=26, img_source='https://i.imgur.com/phBFJkd.gif'),
-    
 ]
 
 # Initialize ultrasonic sensor
@@ -44,24 +42,24 @@ ultrasonic_sensor = UltrasonicSensor(trigger_pin_num=0, echo_pin_num=1)
 # Function to read soil moisture and ultrasonic sensor
 def water_plants():
     if get_reservoir_level() > 20:
-        print("greter")
-        
-        for plant in plants:
-            print(plant.get_name(), plant.get_current_water_level(), plant.get_water_needs())
 
-    
+        for plant in plants:
+            print(plant.get_name(), plant.get_current_water_level(),
+                  plant.get_water_needs())
+
             if plant.get_current_water_level() < plant.get_water_needs():
                 plant.water_self()
-                
+
 
 def get_reservoir_level():
-    
+
     distance = ultrasonic_sensor.read_distance()
     percentage = ultrasonic_sensor.convert_sonic_reading_to_percentage(
         distance)
     return percentage
 
 
+# Function to read sensor data from all plants and ultrasonic sensor
 def read_sensors():
     readings = {}
 
@@ -76,9 +74,8 @@ def read_sensors():
         for plant in plants
     ]
     readings["plants"] = plant_data
-    
 
-    try:
+    try:  # Read ultrasonic sensor
         percentage = get_reservoir_level()
         readings["level"] = percentage
 
@@ -103,6 +100,7 @@ def send_sensor_data():
         response = requests.post(server_url, data=json_data, headers=headers)
         print('HTTP POST Status:', response.status_code)
         response.close()
+
     except Exception as e:
         print("Error sending HTTP POST request:", e)
 
@@ -110,9 +108,8 @@ def send_sensor_data():
 # Send sensor data periodically
 while True:
     send_sensor_data()
-    print("start")
+
+    # Water plants if reservoir level is above 20%
     water_plants()
 
-    time.sleep(10)  # Delay between readings
-    
-
+    time.sleep(3600)  # Delay between readings in seconds
--- server.py
+++ server.py
@@ -7,56 +7,73 @@ import websockets
 # Global variable to store sensor data
 sensor_data = {}
 
+
 # HTTP request handler
 class RequestHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         try:
+
             if self.path == '/' or self.path == '/index.html':
                 # Serve the index.html file
+
                 with open('index.html', 'rb') as file:
                     self.send_response(200)
                     self.send_header('Content-type', 'text/html')
                     self.end_headers()
                     self.wfile.write(file.read())
-            elif self.path == '/styles.css':
-                # Serve the styles.css file
+
+            elif self.path == '/styles.css':  # Serve the styles.css file
+
                 with open('styles.css', 'rb') as file:
                     self.send_response(200)
                     self.send_header('Content-type', 'text/css')
                     self.end_headers()
                     self.wfile.write(file.read())
-            elif self.path == '/script.js':
-                # Serve the script.js file
+
+            elif self.path == '/script.js':  # Serve the script.js file
                 with open('script.js', 'rb') as file:
                     self.send_response(200)
                     self.send_header('Content-type', 'application/javascript')
                     self.end_headers()
                     self.wfile.write(file.read())
-            elif self.path == '/plants':
-                # Serve plants data
+
+            elif self.path == '/plants':  # Serve plants data
+
                 if sensor_data:
                     self.send_response(200)
                     self.send_header('Content-Type', 'application/json')
                     self.end_headers()
-                    self.wfile.write(json.dumps(sensor_data.get('plants', {})).encode('utf-8'))
+                    self.wfile.write(json.dumps(
+                        sensor_data.get('plants', {})).encode('utf-8'))
+
                 else:
                     self.send_error(404, 'Sensor data not available')
+
             elif self.path == '/ultrasonic_reading':
                 # Serve ultrasonic sensor data
+
                 if sensor_data:
                     self.send_response(200)
                     self.send_header('Content-Type', 'application/json')
                     self.end_headers()
-                    self.wfile.write(json.dumps({'level': sensor_data.get('level', 0)}, indent=4).encode())
+                    self.wfile.write(json.dumps(
+                        {
+                            'level': sensor_data.get('level', 0)
+                        }, indent=4).encode())
+
                 else:
                     self.send_error(404, 'Sensor data not available')
+
             else:
                 self.send_error(404, 'File Not Found: %s' % self.path)
+
         except Exception as e:
             print('Error handling GET request:', e)
             self.send_error(500, 'Internal Server Error')
 
     def do_POST(self):
+
         if self.path == '/receive_data':
             content_length = int(self.headers['Content-Length'])
             post_data = self.rfile.read(content_length)
@@ -75,28 +92,33 @@ class RequestHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-Type', 'application/json')
                 self.end_headers()
                 self.wfile.write(b"Data received successfully\r\n")
+
             except json.JSONDecodeError as e:
                 print('Error decoding JSON:', e)
                 self.send_response(400)
                 self.send_header('Content-Type', 'text/plain')
                 self.end_headers()
                 self.wfile.write(b"Error: Invalid JSON data\r\n")
+
             except Exception as e:
                 print('Error processing sensor data:', e)
                 self.send_response(500)
                 self.send_header('Content-Type', 'text/plain')
                 self.end_headers()
                 self.wfile.write(b"Error: Internal Server Error\r\n")
+
         else:
             self.send_response(404)
             self.send_header('Content-Type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Error: Unsupported request\r\n")
 
+
 # WebSocket server handler
 async def handle_websocket(websocket, path):
     try:
         while True:
+
             message = await websocket.recv()
             print(f"Received message from WebSocket client: {message}")
 
@@ -104,40 +126,55 @@ async def handle_websocket(websocket, path):
                 # Send current sensor data to WebSocket client
                 global sensor_data
                 await websocket.send(json.dumps(sensor_data))
+
     except websockets.exceptions.ConnectionClosedError:
         print("WebSocket connection closed")
+
 
 # Function to send data to all WebSocket clients
 async def send_data_to_clients(data):
     connected_clients = asyncio.all_tasks()
+
     for task in connected_clients:
-        if isinstance(task, asyncio.Task) and task.get_coro() == handle_websocket:
+        if isinstance(
+
+                task, asyncio.Task) and task.get_coro() == handle_websocket:
             await task.get_coro()(data)
+
 
 # Function to run the HTTP server
 def run_http_server():
     server_address = ('', 8000)
     httpd = HTTPServer(server_address, RequestHandler)
     print('Starting HTTP server on port 8000...')
+
     try:
         httpd.serve_forever()
+
     except KeyboardInterrupt:
         pass
+
     httpd.server_close()
     print('HTTP server stopped.')
 
+
 # Function to run the WebSocket server
 async def run_websocket_server():
+
     async with websockets.serve(handle_websocket, 'localhost', 8765):
         print('Starting WebSocket server on ws://localhost:8765...')
-        await asyncio.Future()  # Keep the WebSocket server running indefinitely
+        # Keep the WebSocket server running indefinitely
+        await asyncio.Future()
+
 
 # Function to run both servers
 def run_servers():
+
     http_thread = threading.Thread(target=run_http_server)
     http_thread.start()
 
     asyncio.run(run_websocket_server())
+
 
 if __name__ == '__main__':
     run_servers()
